4d0497d7211857cb813dd82776f98aae7348433d
Aiden Seitz <csa.aiden@gmail.com>
2021-02-20 00:10:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,13 @@
+def phrase
+  phrase = "It's-a me, Mario!"
+  return phrase
+end
+
+def status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  return status
+end
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
@@ -6,3 +16,20 @@ end
 def toadstool
   puts status
 end
+
+def link
+  link = "It's Dangerous To Go Alone! Take This."
+  puts link
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
+  
+
+
+
+
+
